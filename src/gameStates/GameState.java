1e9f6feb1b86fdfbe08a290e31508129fed50771
Adam Vongrej <vongrejadam@gmail.com>
2014-05-10 16:53:43 +0200

    Implemented Observer for creeps

--- src/gameStates/GameState.java
+++ src/gameStates/GameState.java
@@ -4,14 +4,17 @@
  */
 package gameStates;
 
+import Listeners.CreepPathUpdatedListener;
 import com.jme3.app.Application;
 import com.jme3.app.SimpleApplication;
 import com.jme3.app.state.AbstractAppState;
 import com.jme3.app.state.AppStateManager;
 import com.jme3.asset.AssetManager;
 import com.jme3.collision.CollisionResults;
+import com.jme3.input.KeyInput;
 import com.jme3.input.MouseInput;
 import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.KeyTrigger;
 import com.jme3.input.controls.MouseButtonTrigger;
 import com.jme3.input.controls.Trigger;
 import com.jme3.light.AmbientLight;
@@ -31,6 +34,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import pathfinder.Constants;
 import pathfinder.Graph;
 import pathfinder.PathFinder;
 import pathfinder.Utils;
@@ -44,10 +48,17 @@ import sun.misc.Queue;
 public class GameState extends AbstractAppState {
 
     private SimpleApplication app;
-    private Node rootNode;
     private Camera cam;
     private AssetManager assetManager;
     private Ray ray;
+    
+    private Node rootNode;
+    private Node creepNode;
+    private Node towerNode;
+    private Node obstacleNode;
+    
+    private List<CreepPathUpdatedListener> creepListeners;
+    
     private PathFinder pathFinder;
     private static final int xBlock = 80;
     private static final int zBlock = 50;
@@ -56,44 +67,56 @@ public class GameState extends AbstractAppState {
     private static Vector3f basePoint;
     private List<Vertex> creepPath;
     private Graph graphBuilder;
-    private CreepControl test;
     private static final Trigger TRIGGER_BUILD = new MouseButtonTrigger(MouseInput.BUTTON_LEFT);
+    private static final Trigger TRIGGER_ACTION_OBSTACLE = new KeyTrigger(KeyInput.KEY_1);
+    private static final Trigger TRIGGER_ACTION_TOWER = new KeyTrigger(KeyInput.KEY_2);
     private static final String MAPPING_BUILD = "Build";
+    private static final String MAPPING_ACTION_OBSTACLE = "Action_Obstacle";
+    private static final String MAPPING_ACTION_TOWER = "Action_Tower";
+    private int action;
     private boolean first = true;
-    private Queue obstacleQueue;
+    
+   
+
 
     @Override
     public void initialize(AppStateManager stateManager, Application app) {
         super.initialize(stateManager, app);
 
-        obstacleQueue = new Queue();
-
         thread.start();
+        creepListeners = new ArrayList<CreepPathUpdatedListener>();
+
+        action = Constants.ACTION_OBSTACLE;
 
         this.app = (SimpleApplication) app;
         this.cam = this.app.getCamera();
         this.rootNode = this.app.getRootNode();
+        this.towerNode = new Node();
+        this.obstacleNode = new Node();
+        this.creepNode = new Node();
         this.assetManager = this.app.getAssetManager();
         ray = new Ray();
+        rootNode.attachChild(creepNode);
+        rootNode.attachChild(towerNode);
+        rootNode.attachChild(obstacleNode);
 
         initLight();
         initConstants();
         initTriggers();
-
-        Geometry platform = generateBox("Platform", xBlock * blockSize, 1f, zBlock * blockSize, "Common/MatDefs/Misc/Unshaded.j3md", ColorRGBA.Blue, Vector3f.ZERO);
-        Geometry base = generateBox("Base", 3 * blockSize, 3 * blockSize, 5 * blockSize, "Common/MatDefs/Misc/Unshaded.j3md", ColorRGBA.Pink, new Vector3f(basePoint.add(new Vector3f(-blockSize * 3f, 3.0f, 0.0f))));
+        initPlatform();
+        initBase();
         generateCreep();
-
-        rootNode.attachChild(platform);
-        rootNode.attachChild(base);
     }
 
     private void initTriggers() {
         app.getInputManager().addMapping(MAPPING_BUILD, TRIGGER_BUILD);
-        app.getInputManager().addListener(actionListener, new String[]{MAPPING_BUILD});
+        app.getInputManager().addMapping(MAPPING_ACTION_OBSTACLE, TRIGGER_ACTION_OBSTACLE);
+        app.getInputManager().addMapping(MAPPING_ACTION_TOWER, TRIGGER_ACTION_TOWER);
+        app.getInputManager().addListener(actionListener, new String[]{MAPPING_BUILD, MAPPING_ACTION_OBSTACLE, MAPPING_ACTION_TOWER});
     }
     private ActionListener actionListener = new ActionListener() {
         public void onAction(String name, boolean isPressed, float tpf) {
+
             if (name.equals(MAPPING_BUILD) && !isPressed) {
                 ray.setOrigin(cam.getLocation());
                 ray.setDirection(cam.getDirection());
@@ -103,12 +126,22 @@ public class GameState extends AbstractAppState {
 
                 if (collisionResults.size() > 0) {
 
-                    //Geometry target = collisionResults.getClosestCollision().getGeometry();
-                    Vector3f contactPoint = collisionResults.getClosestCollision().getContactPoint();
-                    contactPoint.setZ(-contactPoint.getZ());
-                    int[] grid = Utils.calculateGrid(contactPoint);
-                    rootNode.attachChild(generateBox("Obscale", 0.5f, 10f, 0.5f, null, ColorRGBA.Yellow, Utils.calculateCenter(grid[0], grid[1])));
+                    if (action == 1) {
+                        Vector3f contactPoint = collisionResults.getClosestCollision().getContactPoint();
+                        contactPoint.setZ(-contactPoint.getZ());
+                        int[] grid = Utils.calculateGrid(contactPoint);
+                        rootNode.attachChild(generateBox("Obscale", 0.5f, 10f, 0.5f, null, ColorRGBA.Yellow, Utils.calculateCenter(grid[0], grid[1])));
+                    } else if (action == 2) {
+                    }
+
+
                 }
+            }
+            if (name.equals(MAPPING_ACTION_OBSTACLE)) {
+                action = 1;
+            }
+            if (name.equals(MAPPING_ACTION_TOWER)) {
+                action = 2;
             }
         }
     };
@@ -123,6 +156,14 @@ public class GameState extends AbstractAppState {
     private void initConstants() {
         spawnPoint = new Vector3f(-xBlock * blockSize, 1.0f, 0.0f);
         basePoint = new Vector3f(xBlock * blockSize, 1.0f, 0.0f);
+    }
+
+    private void initPlatform() {
+        rootNode.attachChild(generateBox("Platform", xBlock * blockSize, 1f, zBlock * blockSize, "Common/MatDefs/Misc/Unshaded.j3md", ColorRGBA.Blue, Vector3f.ZERO));
+    }
+
+    private void initBase() {
+        rootNode.attachChild(generateBox("Base", 3 * blockSize, 3 * blockSize, 5 * blockSize, "Common/MatDefs/Misc/Unshaded.j3md", ColorRGBA.Pink, new Vector3f(basePoint.add(new Vector3f(-blockSize * 3f, 3.0f, 0.0f)))));
     }
 
     private Geometry generateBox(String name, float x, float y, float z, String matPath, ColorRGBA color, Vector3f pos) {
@@ -145,11 +186,13 @@ public class GameState extends AbstractAppState {
         return geom;
     }
 
+    
     private void generateCreep() {
         Geometry creep = generateBox("Creep", 1f, 1f, 1f, "Common/MatDefs/Misc/Unshaded.j3md", ColorRGBA.Red, spawnPoint.add(new Vector3f(1.0f, 1.0f, 1.0f)));
-        test = new CreepControl(cam, rootNode, basePoint);
-        creep.addControl(test);
-        rootNode.attachChild(creep);
+        CreepControl control = new CreepControl(cam, rootNode, basePoint);
+        addCreepListener(control);
+        creep.addControl(control);
+        creepNode.attachChild(creep);
     }
 
     @Override
@@ -159,7 +202,7 @@ public class GameState extends AbstractAppState {
         if (creepPath != null && first) {
             first = false;
             for (Vertex vertex : creepPath) {
-                rootNode.attachChild(generateBox("Test", 0.5f, 10, 0.5f, null, ColorRGBA.Orange, vertex.getCenter()));
+                rootNode.attachChild(generateBox("Test", 0.05f, 10, 0.05f, null, ColorRGBA.Orange, vertex.getCenter()));
 
             }
             Vertex[][] graph = graphBuilder.getGraph();
@@ -172,17 +215,31 @@ public class GameState extends AbstractAppState {
                 }
             }
         }
-
-        if (!obstacleQueue.isEmpty()) {
-            try {
-                rootNode.attachChild((Geometry) obstacleQueue.dequeue());
-            } catch (InterruptedException ex) {
-                Logger.getLogger(GameState.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-
         super.update(tpf); //To change body of generated methods, choose Tools | Templates.
     }
+    
+    private void notifyCreeps(List<Vertex> path){
+        for (CreepPathUpdatedListener creepPathUpdatedListener : creepListeners) {
+            creepPathUpdatedListener.setPath(path);
+        }
+    }
+    
+    private boolean addCreepListener(CreepControl creep){
+        if (!creepListeners.contains(creep)){
+            creepListeners.add(creep);
+            return true;
+        }
+        return false;
+    }
+    
+     private boolean removeCreepListener(CreepControl creep){
+        if (creepListeners.contains(creep)){
+            creepListeners.remove(creep);
+            return true;
+        }
+        return false;
+    }
+    
     private Thread thread = new Thread() {
         @Override
         public void run() {
@@ -249,7 +306,6 @@ public class GameState extends AbstractAppState {
             obstacles.add(new int[]{139, 58});
             obstacles.add(new int[]{139, 59});
             obstacles.add(new int[]{139, 60});
-
 
 
             obstacles.add(new int[]{60, 0});
@@ -326,9 +382,7 @@ public class GameState extends AbstractAppState {
 
             try {
                 creepPath = pathFinder.calculate(graphBuilder.getGraph(), graphBuilder.getSourceXY(), graphBuilder.getDestinationXY());
-
-
-                test.setCreepPath(creepPath);
+                notifyCreeps(creepPath);       
             } catch (InterruptedException ex) {
                 Logger.getLogger(GameState.class.getName()).log(Level.SEVERE, null, ex);
             }
